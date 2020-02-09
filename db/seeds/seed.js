2fb4d647976b707efb6012bc6310a104ad46ddcd
Hy-M <humayraa.m@outlook.com>
2020-02-09 14:11:16 +0000

    Starting seed functionality

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -3,7 +3,18 @@ const {
     wishlistData
   } = require('../data/index');
 
-  exports.seed = function(knex) {
+const { formatDates } = require('../utils/utils');
 
+  exports.seed = function(knex) {
+    return knex.migrate
+    .rollback()
+    .then(() => knex.migrate.latest())
+    .then(() => {
+      const formattedDatesGardenData = formatDates(gardenData);
+      return knex('garden').insert(formattedDatesGardenData);
+    })
+    .then(() => {
+      return knex('wishlist').insert(wishlistData);
+    })
   };
 
