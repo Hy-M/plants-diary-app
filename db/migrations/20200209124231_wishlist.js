3533f47bec7b4d63fde6d2681b85e2b8f8ac41f4
Hy-M <humayraa.m@outlook.com>
2020-04-01 12:53:38 +0100

    add date_added to wishlist

--- db/migrations/20200209124231_wishlist.js
+++ db/migrations/20200209124231_wishlist.js
@@ -1,13 +1,13 @@
-
 exports.up = function(knex) {
-  return knex.schema.createTable('wishlist', (wishlistTable) => {
-      wishlistTable.increments('plant_id').primary();
-      wishlistTable.string('name').notNullable();
-      wishlistTable.string('image_url');
-      wishlistTable.boolean('is_growing').defaultTo(false);
-  })
+  return knex.schema.createTable("wishlist", wishlistTable => {
+    wishlistTable.increments("plant_id").primary();
+    wishlistTable.string("name").notNullable();
+    wishlistTable.string("image_url");
+    wishlistTable.boolean("is_growing").defaultTo(false);
+    wishlistTable.timestamp("date_added").defaultTo(knex.fn.now());
+  });
 };
 
 exports.down = function(knex) {
-    return knex.schema.dropTable('wishlist');
+  return knex.schema.dropTable("wishlist");
 };
