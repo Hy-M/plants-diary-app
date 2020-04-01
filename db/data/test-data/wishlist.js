0d2f8b071341124f370976a6927676db515b9b5f
Hy-M <humayraa.m@outlook.com>
2020-04-01 13:07:00 +0100

    adadd timestamp in db

--- db/data/test-data/wishlist.js
+++ db/data/test-data/wishlist.js
@@ -1,9 +1,9 @@
 module.exports = [
   {
-    name: "Rose",
+    name: "Peony",
     image_url:
       "https://images.unsplash.com/photo-1561826336-37bdb1339994?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
     is_growing: false,
-    date_added: null
+    date_added: "2016-06-22 19:10:25-07"
   }
 ];
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,21 +1,16 @@
-const {
-    gardenData,
-    wishlistData
-  } = require('../data/index');
+const { gardenData, wishlistData } = require("../data/index");
 
-const { formatDates } = require('../utils/utils');
+const { formatDates } = require("../utils/utils");
 
-
-  exports.seed = function(knex) {
-    return knex.migrate
+exports.seed = function(knex) {
+  return knex.migrate
     .rollback()
     .then(() => knex.migrate.latest())
     .then(() => {
       const formattedDatesGardenData = formatDates(gardenData);
-      return knex('garden').insert(formattedDatesGardenData);
+      return knex("garden").insert(formattedDatesGardenData);
     })
     .then(() => {
-      return knex('wishlist').insert(wishlistData);
-    })
-  };
-
+      return knex("wishlist").insert(wishlistData);
+    });
+};
